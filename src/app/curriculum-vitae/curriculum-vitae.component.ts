bb63766665df467a167d1f2b06aa1e4c6e8ad36d
Cheikh Bassirou MBAYE <cheikhbassirou.mbaye.etu>
2023-12-27 22:03:14 +0100

    cv

--- src/app/curriculum-vitae/curriculum-vitae.component.ts
+++ src/app/curriculum-vitae/curriculum-vitae.component.ts
@@ -1,8 +1,10 @@
-import { experiencesFr } from './../api/experiencesFr';
 import { Component, OnInit } from '@angular/core';
 import * as awesom from '@fortawesome/free-solid-svg-icons';
 import { TranslationLoaderService } from '../service/translation-loader.service';
+import { locale as english } from '../shared/i18n/en';
 import { locale as french } from '../shared/i18n/fr';
+import { experiencesFr} from '../api/experiencesFr';
+import { experiencesEn} from '../api/experiencesEn';
 import { TranslateService } from '@ngx-translate/core';
 @Component({
   selector: 'app-curriculum-vitae',
@@ -11,16 +13,16 @@ import { TranslateService } from '@ngx-translate/core';
 
 })
 export class CurriculumVitaeComponent implements OnInit {
-  experiences:any=experiencesFr;
+  experiences:any=experiencesEn;
   planeIcon: any ;
   gameIcon:any;
   volleyBallIcon:any;
 
   constructor(private _translationLoaderService: TranslationLoaderService,private _translateService: TranslateService) {
-    this._translationLoaderService.loadTranslations(french, french);
+    this._translationLoaderService.loadTranslations(english, french);
     this._translateService.onLangChange.subscribe(()=>{
       if(this._translateService.currentLang=="en"){
-        this.experiences=experiencesFr;
+        this.experiences=experiencesEn;
       }
       else{
         this.experiences=experiencesFr;
